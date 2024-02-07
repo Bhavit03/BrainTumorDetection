4890ccb5f834c5a6e3c94a18232d4a6e9d6a7789
Bhavit Karnatak <karnbhavit2003@gmail.com>
2024-02-07 15:25:28 +0530

    prepare base model compeleted

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from CNNclassifier import logger
 from CNNclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from CNNclassifier.pipeline.stage_02_prepare_basemodel import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} compeleted <<<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>> stage {STAGE_NAME} compeleted <<<<\n\nx=====x")
 except Exception as e:
     logger.exception(e)
